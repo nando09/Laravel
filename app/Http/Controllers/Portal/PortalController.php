485e4aa212187a1b64cb10dd42596f508abff7dc
nando09 <nando.online.fb@gmail.com>
2018-09-03 22:39:38 -0300

    Liga a pagina com o login, se esta logado ou nao.

--- app/Http/Controllers/Portal/PortalController.php
+++ app/Http/Controllers/Portal/PortalController.php
@@ -6,8 +6,8 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class PortalController extends Controller{
-    //
-    public function index(){
-    	return view('portal.home.index');
-    }
+	//
+	public function index(){
+		return view('portal.home.index');
+	}
 }
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,18 @@
 <?php
 
-Route::get('/', 'Portal\PortalController@index');
-// $this->get('/', 'Portal\PortalController@index');
+// Criando um grupo para analisar se esta dentro dos conformes para acessar essa barra
+$this->group(['middleware' => ['auth'], 'namespace' => 'Admin'], function(){
+
+	// Route::get('admin, 'Admin\AdminController@index');
+	$this->get('admin', 'AdminController@index')->name('admin.home');
+});
+
+
+// Route::get('/', 'Portal\PortalController@index');
+$this->get('/', 'Portal\PortalController@index')->name('home');
+
+
 
 Auth::routes();
- 
-Route::get('/home', 'HomeController@index')->name('home');
+
+// Route::get('/home', 'HomeController@index')->name('home');
