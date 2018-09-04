eb68899cf26bec7d68ebcbebb850c5222ae12e94
nando09 <nando.online.fb@gmail.com>
2018-09-03 22:41:17 -0300

    Adiciona arquivos novos para a organização

---
+++ resources/views/admin/home/index.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'Home Dasboard')
+
+@section('content_header')
+    <h1 class="text-center">Fernando Batista</h1>
+@stop
+
+@section('content')
+    <p class="text-center">Muito lindo esse menino!</p>
+@stop
