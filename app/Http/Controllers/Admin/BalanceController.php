2bac754c1ccf9052179408321241cb57cba4feec
nando09 <nando.online.fb@gmail.com>
2018-09-15 00:03:40 -0300

    Novos arquivos para o admin

---
+++ app/Http/Controllers/Admin/BalanceController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class BalanceController extends Controller
+{
+	//
+	public function index(){
+		// dd(auth()->user()->balance()->get());
+		$balance = auth()->user()->balance;
+		$amount = $balance ? $balance->amount : 0;
+
+		return view('admin.balance.index', compact('amount'));
+	}
+}
---
+++ resources/views/admin/balance/index.blade.php
@@ -0,0 +1,46 @@
+@extends('adminlte::page')
+
+@section('title', 'Home Dasboard')
+
+@section('content_header')
+	<h1 class="text-center">Saldo</h1>
+
+	<ol class="breadcrumb">
+		<li><a href="">Dashboard</a></li>
+		<li><a href="">Saldo</a></li>
+	</ol>
+@stop
+
+@section('content')
+	<div class="box">
+		<div class="box-header">
+			<a href="" class="btn btn-primary"><i class="fa fa-cart-plus"></i> Recarregar</a>
+			<a href="" class="btn btn-danger"><i class="fa fa-cart-arrow-down"></i> Sacar</a>
+		</div>
+		<div class="box-header">
+			<div class="small-box bg-green">
+				<div class="inner">
+					<h3>
+						<font style="vertical-align: inherit;">
+							<font style="vertical-align: inherit;">R$ </font>
+						</font>
+						<sup style="font-size: 20px">
+							<font style="vertical-align: inherit;">
+								<font style="vertical-align: inherit;">{{ number_format($amount, 2, ',', '') }}</font>
+							</font>
+						</sup>
+					</h3>
+				</div>
+				<div class="icon">
+					<i class="ion ion-cash"></i>
+				</div>
+				<a href="#" class="small-box-footer">
+					<font style="vertical-align: inherit;">
+						<font style="vertical-align: inherit;">Hístorico</font>
+					</font>
+					<i class="fa fa-arrow-circle-right"></i>
+				</a>
+			</div>
+		</div>
+	</div> 
+@stop
